31aeb619edf6b82e299d542bf8089849be96d807
MARSHAl RAFI <48475717+rafitricker@users.noreply.github.com>
2019-05-15 09:07:53 +0000

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,68 @@
+import os
+import time
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
+green = '\033[32;1m'
+
+gta = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+
+white = '\033[1;37m' # White 
+red = '\033[31m' # red
+orange = '\033[33m' # orange
+blue = '\033[34m' # blue
+p  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+green = ("\033[92m")
+pryellow = ("\033[93m")
+prLightPurple=("\033[94m")
+prCyan=("\033[96m")
+prgray = ("\033[97m")
+prBlack=("\033[98m")
+
+
+class banner:
+    def __init__(self):
+        print pryellow+"""
+          .AMMMMMMMMMMA.
+       .AV. :::.:.:.::MA.
+      A  :..        : .: A
+     A ..              .  A.
+    A  :.    :::::::::  : : A
+    M  .    :::.:.:.:::  . .M
+    M  :   ::.:.....::.:   .M
+    V : :.::.:........:.:  :V
+   A  A:    ..:...:...:.   A A
+  .V  MA:.....:M.::.::. .:AM.M
+ A   .VMMMMMMMMM:.:AMMMMMMMV: A
+:M .  . VMMMMMMV.:A  VMMMMV .:M:
+ V.:.  .. VMMMV.:AM.. VMV  .: V
+  V.  .:. .....:AMMA. . .:. .V
+   VMM...: ...:.MMMM.: .: MMV
+        VM: . ..M.:M..:::M
+          M::. .:.... .::M       AFGHAN PASSLIST
+          M:.  :. .... ..M       MARSHAL RAFI
+          V:  M:. M. :M .V
+           V.:M.. M. :M.V
+[PASSWORD LIST SAZ HERFAWI 2019]
+[@Created By RAFI TRICKER] """+C+p+"""
+web site : www.rafitricker.blogsky.com
+facebook : rafi.569
+github: https://github.com/rafitricker
+dev: MarShal RaFi
+Phone Number:+93795679595 +93796706345
+program type: Python
+       
+       """
+        print (red+'-')*43
+        
+        
+        
+       
